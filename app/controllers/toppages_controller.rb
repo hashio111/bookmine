62dfc00ab77f968ead746c43f59d471f2297dd57
hashio111 <mnb7112es@gmail.com>
2019-11-07 17:49:50 +0900

    indexアクションを追加

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,10 @@
 class ToppagesController < ApplicationController
   def index
+    url = 'https://www.googleapis.com/books/v1/volumes?q='
+    request = url + "rails"
+    enc_str = URI.encode(request)
+    uri = URI.parse(enc_str)
+    json = Net::HTTP.get(uri)
+    @books = JSON.parse(json)
   end
 end
