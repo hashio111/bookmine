f92ee695886dbe131d5e7f925d30623c6d5f75c9
hashio111 <mnb7112es@gmail.com>
2019-11-25 22:19:17 +0900

    showページを修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,7 @@ class LikesController < ApplicationController
   def create
     like = current_user.likes.build(like_params)
     if like.save
-      flash[:success] = "お気に入り登録をしました"
+      flash[:success] = "お気に入りに登録しました"
       redirect_back(fallback_location: root_path)
     else
       flash[:danger] = "お気に入り登録に失敗しました"
