994ed60dc5deb8408b5907bfe63d510e8c2656c1
hashio111 <mnb7112es@gmail.com>
2019-11-27 21:34:59 +0900

    インスタンス変数を@productsから@likesに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.page(params[:page]).per(10)
+    @users = User.page(params[:page]).per(12)
   end
 
   def show
     @user = User.find_by(id: params[:id])
-    @products = @user.products
+    @likes = @user.products
+
   end
 end
