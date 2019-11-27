b145da6a514ad75fb52932dc4c9ef15c040bdbb3
hashio111 <mnb7112es@gmail.com>
2019-11-27 21:18:22 +0900

    フォロー機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,17 @@ Rails.application.routes.draw do
 
   root 'toppages#index'
 
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show] do
+    member do
+      get :following
+      get :followers
+    end
+  end
   resources :products, only: %i[index new show] do
     collection do
       get :product_registration
     end
   end
-
   resources :likes, only: %i[create destroy]
+  resources :relationships, only: %i[create destroy]
 end
