4448834b29c75644545d43739f6a9644a1ceea73
hashio111 <mnb7112es@gmail.com>
2019-12-02 17:42:07 +0900

    rubocopでコード整型

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,6 +3,6 @@ class ToppagesController < ApplicationController
     @user = User.new unless user_signed_in?
     @posts = Post.limit(3).order(created_at: :desc)
     product_ids = Like.group(:product_id).order("count_product_id DESC").limit(3).count(:product_id).keys
-    @ranking = product_ids.map{|id| Product.find(id)}
+    @ranking = product_ids.map { |id| Product.find(id) }
   end
 end
