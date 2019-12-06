f7fadccf3bfec97d75dcb8ef0e0cbed95dc0aa95
hashio111 <mnb7112es@gmail.com>
2019-12-06 19:45:20 +0900

    rankingのインスタンス変数を定義

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,4 +1,12 @@
 class RankingsController < ApplicationController
+  before_action :set_user, only: %i[index]
   def index
+    likes_product_ids = Like.group(:product_id).order("count_product_id DESC").count(:product_id).keys
+    likes_arr = likes_product_ids.map { |id| Product.find(id) }
+    @likes_ranking = Kaminari.paginate_array(likes_arr).page(params[:page]).per(2)
+    posts_product_ids = Post.group(:product_id).order("count_product_id DESC").count(:product_id).keys
+    posts_arr = posts_product_ids.map { |id| Product.find(id) }
+    @posts_ranking = Kaminari.paginate_array(posts_arr).page(params[:page]).per(2)
+    return unless request.xhr?
   end
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,6 @@
 class ToppagesController < ApplicationController
+  before_action :set_user, only: %i[index]
   def index
-    @user = User.new unless user_signed_in?
     @posts = Post.limit(3).order(created_at: :desc)
     product_ids = Like.group(:product_id).order("count_product_id DESC").limit(3).count(:product_id).keys
     @ranking = product_ids.map { |id| Product.find(id) }
